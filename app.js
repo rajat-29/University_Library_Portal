0dd55dc3e4c014f8ca79510e63fa37399b813750
rajat-29 <rajat295gupta@gmail.com>
2019-07-19 06:04:05 +0530

    login page

---
+++ app.js
@@ -0,0 +1,57 @@
+var express = require('express')
+var path = require('path')
+var app = express()
+var session = require('express-session');
+var ejs = require('ejs');
+var mongodb = require('mongodb');
+var MongoDataTable = require('mongo-datatable');
+app.use(passport.initialize());
+app.use(passport.session());
+ObjectId = require('mongodb').ObjectID;
+var MongoClient = mongodb.MongoClient;
+var userdata = new Object();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'Views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname,'/public'))) /*folder path*/
+app.use(express.static(path.join(__dirname,'public/uploads')));
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())									/*include express*/
+app.use(session({
+    secret: "xYzUCAchitkara",
+    resave: false,
+    saveUninitialized: true,
+}))
+
+var mongoose = require('mongoose');						/*include mongo*/
+var mongoDB = 'mongodb://localhost/blooddonation';
+
+mongoose.set('useFindAndModify', false);
+mongoose.connect(mongoDB,{ useNewUrlParser: true});
+
+mongoose.connection.on('error',(err) => {					/*database connect*/
+    console.log('DB connection Error');
+})
+
+mongoose.connection.on('connected',(err) => {
+    console.log('DB connected');
+})
+
+// user data base scehema //
+var userSchema = new mongoose.Schema({					/*define structure of database*/
+    name: String,
+    email: String,
+    password: String,
+    phone: String,
+    city: String,
+    gender: String,
+    dob: String,
+    role: String,   
+    status: String,
+    flag: Number, 
+})
+
+var users = mongoose.model('blooddonation', userSchema);
---
+++ public/js/script.js
@@ -0,0 +1,68 @@
+// index page for login page //
+
+var user_name = document.getElementById('user_name');
+var pass = document.getElementById('pass');
+var submit = document.getElementById('submit');
+
+submit.addEventListener("click", function() {
+	//console.log(pass.value);
+	var request = new XMLHttpRequest();
+    request.open('POST',"/checkLogin");
+    request.setRequestHeader("Content-Type","application/json");
+    request.send(JSON.stringify({name : n.value,password: pass.value}));
+    request.addEventListener("load",function() {
+    	var data = request.responseText;
+    	if(data === 'true') {
+    		//console.log('hello user');
+            window.location = "/home";
+            //window.location.href = "h.html";
+        }
+        else if(data === 'false')
+        {
+            window.location = "/404";
+        }
+    	else {
+            document.getElementById("wrong_id").style.display = "block";
+            document.getElementById("forming").style.height = "340px";
+    		console.log('getout');
+            n.value = "";
+            pass.value = "";
+    	}
+      //  console.log(data);
+    });
+})
+
+
+pass.addEventListener("keyup", function() {
+
+     if (event.keyCode === 13) {
+    //console.log(pass.value);
+    var request = new XMLHttpRequest();
+    request.open('POST',"/checkLogin");
+    request.setRequestHeader("Content-Type","application/json");
+    request.send(JSON.stringify({name : n.value,password: pass.value}));
+    request.addEventListener("load",function() {
+        var data = request.responseText;
+        if(data === 'true') {
+            console.log('hello user');
+            window.location = "/home";
+            //window.location.href = "h.html";
+        }
+        else if(data === 'false')
+        {
+            window.location = "/404";
+        }
+        else {
+            document.getElementById("wrong_id").style.display = "block";
+            document.getElementById("forming").style.height = "340px";
+            console.log('getout');
+            n.value = "";
+            pass.value = "";
+        }
+      //  console.log(data);
+    });
+}
+})
+
+
+
