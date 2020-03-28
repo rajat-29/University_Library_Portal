23bf4be7797fc1dba826bc6bd076d7395c92ee28
Rajat Gupta <rajat295gupta@gmail.com>
2020-03-28 10:55:28 +0530

    topbar

--- public/js/userdashboard.js
+++ public/js/userdashboard.js
@@ -1,24 +1,3 @@
-function openissuedBookSpecificUser()
-{
-    window.location = '/user/openissuedBookSpecificUser';
-}
-
-function updateUserProfile()
-{
-    window.location = '/user/updateUserProfile';
-}
-
-function valid()
-{
-    if(document.chngpwd.newpassword.value!= document.chngpwd.confirmpassword.value)
-    {
-        alert("New Password and Confirm Password Field do not match  !!");
-        document.chngpwd.confirmpassword.focus();
-        return false;
-    }
-    return true;
-}
-
 function fetchNumbers()
 {
     var totalissuedBooksToUser = document.getElementById('totalissuedBooksToUser');
@@ -33,38 +12,4 @@ function fetchNumbers()
         countBookstoUser = JSON.parse(request5.responseText);
         totalissuedBooksToUser.innerHTML = countBookstoUser;
     }
-}
-
-function changePassword()
-{
-    window.location="/admin/changePassword";
-}
-
-// render home page
-function home_page()
-{
-    window.location = "/login/home";
-}
-
-// logout user
-function openlogoutpage()
-{
-    $.confirm({
-    theme: 'supervan',
-    title: 'Confirm Logout!',
-    content: 'Do you really want logout?',
-    draggable: true,
-    buttons: {
-        Yes: {
-            action: function () {
-             window.location = "/admin/logout_person";
-        }
-    },
-        No: {
-             action: function () {
-            
-        }
-    },
-    }
-    });
 }
