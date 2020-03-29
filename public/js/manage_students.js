009bd3d7c94d2fcdc3285b7521223af3fce6b16a
Rajat Gupta <rajat295gupta@gmail.com>
2020-03-29 13:26:32 +0530

    manage staff

--- public/js/manage_students.js
+++ public/js/manage_students.js
@@ -11,10 +11,10 @@
       },
       "columns": [
       {
-        "data" : "uniId"
+        "data" : "uniId", "orderable": false
       },
       {
-        "data" : "name"
+        "data" : "name", "orderable": false
       },
       {
         "data" : "phone", "orderable": false
@@ -23,49 +23,26 @@
         "data" : "role", "orderable": false
       },
       {
-        "data" : null
+        "data" : null, "orderable": false
       },
       ],
       "columnDefs": [{
                 "targets": -1,
 
                 "render": function (data, type, row, meta) {
-                   return '<span class="btn btn-danger btn-sm emailbtn actionbtns" id="delete" onclick=deleteTag("'+row._id+'")><i class="fas fa-trash"></i></span>';    
+                   return '<span class="btn btn-danger btn-sm" onclick=deleteTag("'+row._id+'")><i class="fas fa-trash"></i></span>';    
           }
-            }],
+      }],
     });
-  });
+});
 
-  function deleteTag(ides)
-  {
-   $(document).on("click", "#delete", function() {
-    d = $(this).parent().parent()[0].children;
-  $.confirm({
-      title: 'Delete Category!',
-      content: "Are you sure you want to delete " + d[1].innerHTML,
-      draggable: true,
-      buttons: {
-        Yes: {
-             btnClass: 'btn-success any-other-class',
-              action: function () {
-               btnClass: 'btn-red any-other-class'
-               var filename = '/admin/students/' + ides;
-            
-               var request = new XMLHttpRequest();
-               request.open('DELETE',filename);
-               request.send()
-               request.addEventListener("load",function(event)
-              {
-                  table.ajax.reload(null, false);
-              });  
-          }
-      },
-        No: {
-            btnClass: 'btn-danger any-other-class',
-             action: function () {      
-          }
-      },
-      }
-    });
-})
+function deleteTag(ides) {
+
+    var filename = '/admin/students/' + ides; 
+    var request = new XMLHttpRequest();
+    request.open('DELETE',filename);
+    request.send()
+    request.addEventListener("load",function(event) {
+        table.ajax.reload(null, false);
+    });  
 }
