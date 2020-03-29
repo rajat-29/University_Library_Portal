34df8e2349c132d506032966fb52210480cbf9c6
Rajat Gupta <rajat295gupta@gmail.com>
2020-03-29 19:38:59 +0530

    change pass update

--- public/js/changePassword.js
+++ public/js/changePassword.js
@@ -1,28 +1,24 @@
-var submitbtn = document.getElementById('submitbtn');
-
 submitbtn.addEventListener("click", function() {
 
-	var oldPass = document.getElementById('oldPass');
-	var newPass = document.getElementById('newPass');
-	var confPass = document.getElementById('confPass');
+	document.getElementById("field_info").style.display = 'visible';
+	document.getElementById("field_info").style.display = 'block';
+	document.getElementById("field_info").style.marginTop = '10px';
+	document.getElementById("field_info").style.marginBottom = '10px';
 
-	if(oldPass.value == '' || newPass.value == '' || confPass.value == '')
-	{
-		alert("Field is Empty");
+	if(oldPass.value == '' || newPass.value == '' || confPass.value == '')	{
+		display_password.innerHTML= "Field is Empty";
 		return;
 	}
 
-	if(newPass.value != confPass.value)
-	{
-		alert("Confirm Password should Match");
+	if(newPass.value != confPass.value) {
+		display_password.innerHTML= "Confirm Password should Match !!";
 		return;
 	}
-	else if(newPass.value.length < 6)
-	{
-		alert("Password Length is Short");
+	else if(newPass.value.length < 6) {
+		display_password.innerHTML= "Password Length is Short";
+		return;
 	}
-	else
-	{
+	else {
 		var obj = new Object();
 		obj.oldpass = oldPass.value;
 		obj.newpass = newPass.value;
@@ -31,16 +27,13 @@ submitbtn.addEventListener("click", function() {
 		request.open('POST', '/admin/changePasswordDatabase');
 	    request.setRequestHeader("Content-Type","application/json");
 	    request.send(JSON.stringify(obj))
-	    request.onload = function ()
-	    {
-	    	if(request.responseText == "true")
-	    	{
-	    		alert("Password Changed Successfully");
-	    		 window.location = "/admin/changePassword";
+	    request.onload = function () {
+	    	if(request.responseText == "true")	{
+	    		display_password.innerHTML= "Password Changed Successfully !!";
+	    		location.reload();
 	    	}
 	    	else
-	    		alert(request.responseText)
+	    		display_password.innerHTML= request.responseText;
 	    }  
 	}
-   
 })
